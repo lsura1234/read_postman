acdf8879f044615a6be8f81b425865edec8df196
allee2470@gmail.com <allee2470@gmail.com>
2019-06-13 14:12:56 +0700

    success v1

--- read.js
+++ read.js
@@ -1,9 +1,35 @@
-const beforData = require('./data.json')
-let afterData = {}
-console.log('data ',beforData)
+const beforData = require("./data.json");
+const { get } = require("lodash");
+let afterData = {};
+afterData.title = { collectionName: beforData.info.name };
+afterData.title = {
+  ...afterData.title,
+  apiName: beforData.item.map((value, index) => {
+    return value.name;
+  })
+};
+afterData = {
+  ...afterData,
+  item: beforData.item.map((value, index) => {
+    const { request } = value;
+    
+    let bodyJson = request.body.raw 
+      ? JSON.parse(
+          request.body.raw
+            .replace(/\n/gi, "")
+            .replace(/\t/gi, "")
+            .replace(/\"/gi, '"')
+        )
+      : "";
+    let respData = {
+      name: value.name,
+      method: request.method,
+      url: request.url.raw,
+      headerField: request.header,
+      bodyField: bodyJson
+    };
+    return respData;
+  })
+};
 
-
-
-
-
-
+console.log("after , ", JSON.stringify(afterData, null, 2));
